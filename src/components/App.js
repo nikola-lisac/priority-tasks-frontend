0db3fb54a98336dc9af12013b3dd58f813abb335
BojanJakic <bojanjakic1@gmail.com>
2017-08-18 18:55:06 +0200

    #8 Add Task component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Task from './Task'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Priority Tasks</h1>
+        <Task />
       </div>
 
     );
