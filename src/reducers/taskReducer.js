2a431d96f93a3abd38f4e6557d210a600cc91f24
BojanJakic <bojanjakic1@gmail.com>
2017-08-18 18:59:51 +0200

    #4 Create switch statement, add SAVE_TASK case

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,5 +1,18 @@
 const taskReducer = (state = {}, action) => {
-    return state;
+
+    switch (action.type) {
+        case 'SAVE_TASK' :
+            /*
+            * I'm not sure what to do here, because i don't know exactly how UI would looks like
+            * I presume that i should update state by adding newly created task to the existing task's list
+            * If so, i need #14-Get_All_Task ticket done to accomplish this piece of code
+            */
+            return state;
+
+        default :
+            return state;
+    }
+
 };
 
 export default taskReducer;
