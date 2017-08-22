48b958cc9cf903196372d8803e7fc8d324fc1d85
BojanJakic <bojanjakic1@gmail.com>
2017-08-22 20:45:30 +0200

    #12 Update state within ALL_TASKS case

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,17 +1,17 @@
 import { SAVE_TASK } from '../actions/type';
 import { ALL_TASKS } from '../actions/type';
 
-let initState = {
+const INIT_STATE = {
     allTasks: []
 };
 
-const taskReducer = (state = initState, action) => {
+const taskReducer = (state = INIT_STATE, action) => {
     switch (action.type) {
         case SAVE_TASK :
             return state;
 
         case ALL_TASKS :
-            return state;
+            return { ...state, allTasks : action.payload };
 
         default :
             return state;
