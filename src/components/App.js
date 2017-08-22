3a10359a63ff1181e99916a8f4b124c48c2ad2ce
BojanJakic <bojanjakic1@gmail.com>
2017-08-22 21:12:48 +0200

    #12 Change order of components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TasksList from '../containers/tasksList'
 import Task from '../containers/task'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Priority Tasks</h1>
-        <TasksList />
-        <Task />
-      </div>
-
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Priority Tasks</h1>
+                <Task />
+                <TasksList />
+            </div>
+        );
+    }
 }
 
 export default App;
