092a884c880c17bd76c5f6306f224936942b3dbd
BojanJakic <bojanjakic1@gmail.com>
2017-08-22 18:19:51 +0200

    #12 Add <TaskList /> component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TasksList from './tasksList'
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div>
         <h1>Priority Tasks</h1>
+        <TasksList />
       </div>
 
     );
