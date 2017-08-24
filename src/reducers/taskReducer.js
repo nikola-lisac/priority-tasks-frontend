137d61a1ac85f0b279623d290468187499c0b767
BojanJakic <bojanjakic1@gmail.com>
2017-08-24 16:38:32 +0200

    #12 Avoid spread operator, delete INIT_STATE variable

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,17 +1,13 @@
-import { SAVE_TASK } from '../actions/type';
-import { ALL_TASKS } from '../actions/type';
+import {SAVE_TASK} from '../actions/type';
+import {ALL_TASKS} from '../actions/type';
 
-const INIT_STATE = {
-    allTasks: []
-};
-
-const taskReducer = (state = INIT_STATE, action) => {
+const taskReducer = (state = [], action) => {
     switch (action.type) {
         case SAVE_TASK :
-            return { ...state, allTasks : state.allTasks.concat(action.payload)};
+            return Object.assign([], state.concat(action.payload));
 
         case ALL_TASKS :
-            return { ...state, allTasks : action.payload };
+            return action.payload;
 
         default :
             return state;
