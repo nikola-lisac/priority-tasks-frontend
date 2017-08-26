7244ae909495b320ced68b28f6a88a2f08ad1d35
BojanJakic <bojanjakic1@gmail.com>
2017-08-26 17:27:20 +0200

    #12 Change Object.assign(...) -> [action.payload, ...state]

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,12 +1,12 @@
 import {SAVE_TASK} from '../actions/type';
-import {ALL_TASKS} from '../actions/type';
+import {GET_ALL_TASKS} from '../actions/type';
 
 const taskReducer = (state = [], action) => {
     switch (action.type) {
         case SAVE_TASK :
-            return Object.assign([], state.concat(action.payload));
+            return [action.payload, ...state];
 
-        case ALL_TASKS :
+        case GET_ALL_TASKS :
             return action.payload;
 
         default :
