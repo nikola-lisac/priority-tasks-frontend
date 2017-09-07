7e1be14fb19c59ec52ffc8054e603d53b6a34c99
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-07 09:06:03 +0200

    #13 Migrate from React.PropTypes

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 const TaskItem = (props) => (
         <li>TASK NAME : {props.task.name}</li>
 );
 
 TaskItem.propTypes = {
-    task: React.PropTypes.object.isRequired,
+    task: PropTypes.object.isRequired,
 };
 
 export default TaskItem;
