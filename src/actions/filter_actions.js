5e36b4198d285a41ef3cce87a1eef43e5a391c89
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-20 13:29:14 +0200

    #10 Create filter action type SET VISIBILITY FILTER

---
+++ src/actions/filter_actions.js
@@ -0,0 +1,11 @@
+import {SET_VISIBILITY_FILTER} from './type'
+
+
+export const setVisibilityFilter = filter => {
+    return (dispatch) => {
+        dispatch({
+            type: SET_VISIBILITY_FILTER,
+            filter
+        })
+    }
+};
--- src/actions/type.js
+++ src/actions/type.js
@@ -2,3 +2,4 @@ export const SAVE_TASK = 'SAVE_TASK';
 export const GET_ALL_TASKS = 'GET_ALL_TASKS';
 export const POSTPONE_TASK = 'POSTPONE_TASK';
 export const COMPLETE_TASK = 'COMPLETE_TASK';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
