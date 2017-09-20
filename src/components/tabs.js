25d114f93d931b5965b8c68be70eab55055bc0eb
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-20 19:03:22 +0200

    #10 Refactor code

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -3,15 +3,15 @@ import FilterLink from '../containers/filterLink';
 
 const Tabs = () => (
     <div className="right-tabs clearfix">
-    <ul className="nav nav-tabs">
-        <FilterLink filter="TODAY">
-            <h4>Today</h4>
-        </FilterLink>
-        {" "}
-        <FilterLink filter="TOMORROW">
-            <h4>Tomorrow</h4>
-        </FilterLink>
-    </ul>
+        <ul className="nav nav-tabs">
+            <FilterLink filter="TODAY">
+                <h4>Today</h4>
+            </FilterLink>
+            {" "}
+            <FilterLink filter="TOMORROW">
+                <h4>Tomorrow</h4>
+            </FilterLink>
+        </ul>
     </div>
 );
 
