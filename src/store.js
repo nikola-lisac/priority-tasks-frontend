5e2389a6bd4bd081f1c442b1860438d7adc234f1
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-20 13:42:41 +0200

    #10 Implement combineReducers in store.js

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import taskReducer from './reducers/taskReducer';
+import filterReducer from './reducers/filterReducer';
 import thunk from 'redux-thunk'
 import logger from 'redux-logger';
 
 const store = createStore(
-    taskReducer,
+    combineReducers({
+        taskReducer,
+        filterReducer
+    }),
     applyMiddleware(
         logger(),
         thunk
