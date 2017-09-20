ac7c93d1be3686e29691e680beb48a236981fa9a
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-20 19:05:18 +0200

    #10 Refactor code

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -7,7 +7,6 @@ const Tabs = () => (
             <FilterLink filter="TODAY">
                 <h4>Today</h4>
             </FilterLink>
-            {" "}
             <FilterLink filter="TOMORROW">
                 <h4>Tomorrow</h4>
             </FilterLink>
