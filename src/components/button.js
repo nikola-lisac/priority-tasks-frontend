4b06818bec500c772ead0f912974a954b82abca6
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-20 13:58:56 +0200

    #10 Refactor Button component

--- src/components/button.js
+++ src/components/button.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = (props) => (
     <button
-        type="button"
         key={props.key}
-        className={props.completed ? 'hidden' : 'btn btn-blue'}
+        className={
+            props.complete ?
+                'hidden' :
+                props.text === 'Postpone' ?
+                    'glyphicon glyphicon-time' :
+                    'glyphicon glyphicon-trash'}
         onClick={props.onClick}
-        disabled={props.completed}
+        disabled={props.complete}
     >
         {props.text}
     </button>
 );
+
+Button.propTypes = {
+    complete: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired
+};
 
 export default Button;
