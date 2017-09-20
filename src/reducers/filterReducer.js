55c19360ef1f3aba569680e1322de5099c900d90
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-09-20 13:37:53 +0200

    #10 Create filterReducer

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,14 @@
+import {SET_VISIBILITY_FILTER} from '../actions/type';
+
+
+const filterReducer= (state = 'TODAY', action) => {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER :
+            return action.filter;
+
+        default:
+            return state
+    }
+};
+
+export default filterReducer;
