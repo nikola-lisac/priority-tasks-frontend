80710ab9e782f9baafb6d3b0bd936d997fa81ab0
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-05 22:43:20 +0200

    #25 Reformat code

--- src/components/taskItem.js
+++ src/components/taskItem.js
@@ -5,25 +5,25 @@ const TaskItem = (props) => (
     <div className="col-sm-12 d-inline-flex task">
         <div className="col-sm-7 d-flex justify-content-left">
             <p className={props.task.completed ?
-            'complete-task' : 'none'}>
+                "complete-task" : "none"}>
             {props.task.name}
             </p>
         </div>
         <div className="col-sm-5 d-flex justify-content-end">
             <Button
                 className={props.task.completed ?
-                    'btn btn-default btn-sm glyphicon glyphicon-remove-circle' :
-                    'btn btn-default btn-sm glyphicon glyphicon-ok-circle'}
+                    "btn btn-default btn-sm glyphicon glyphicon-remove-circle' :
+                    "btn btn-default btn-sm glyphicon glyphicon-ok-circle"}
                 onClick={props.onToggleHandler}
             />
             <Button
                 className={props.task.completed || props.task.postpone ?
-                    'hidden' :
-                    'btn btn-default btn-sm glyphicon glyphicon-time'}
+                    "hidden" :
+                    "btn btn-default btn-sm glyphicon glyphicon-time"}
                 onClick={props.onPostponeHandler}
             />
             <Button
-                className='btn btn-default btn-sm glyphicon glyphicon-trash'
+                className="btn btn-default btn-sm glyphicon glyphicon-trash"
                 onClick={props.onDeleteHandler}
             />
         </div>
