0601061005578108de29644a2b1979d047f6e79b
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-05 23:57:16 +0200

    #25 Make TaskItem responsive

--- src/components/taskItem.js
+++ src/components/taskItem.js
@@ -2,30 +2,32 @@ import React from 'react';
 import Button from "./button";
 
 const TaskItem = (props) => (
-    <div className="col-sm-12 d-inline-flex task">
-        <div className="col-sm-7 d-flex justify-content-left">
-            <p className={props.task.completed ?
-                "complete-task" : "none"}>
-            {props.task.name}
-            </p>
-        </div>
-        <div className="col-sm-5 d-flex justify-content-end">
-            <Button
-                className={props.task.completed ?
-                    "btn btn-default btn-sm glyphicon glyphicon-remove-circle' :
-                    "btn btn-default btn-sm glyphicon glyphicon-ok-circle"}
-                onClick={props.onToggleHandler}
-            />
-            <Button
-                className={props.task.completed || props.task.postpone ?
-                    "hidden" :
-                    "btn btn-default btn-sm glyphicon glyphicon-time"}
-                onClick={props.onPostponeHandler}
-            />
-            <Button
-                className="btn btn-default btn-sm glyphicon glyphicon-trash"
-                onClick={props.onDeleteHandler}
-            />
+    <div className="row task">
+        <div className="col-sm-12 d-inline-flex">
+            <div className="col-sm-9 d-flex justify-content-left">
+                <p className={props.task.completed ?
+                    "complete-task" : "none"}>
+                    {props.task.name}
+                </p>
+            </div>
+            <div className="col-sm-3 d-flex justify-content-end">
+                <Button
+                    className={props.task.completed ?
+                        "btn btn-default btn-sm glyphicon glyphicon-remove-circle" :
+                        "btn btn-default btn-sm glyphicon glyphicon-ok-circle"}
+                    onClick={props.onToggleHandler}
+                />
+                <Button
+                    className={props.task.completed || props.task.postpone ?
+                        "hidden" :
+                        "btn btn-default btn-sm glyphicon glyphicon-time"}
+                    onClick={props.onPostponeHandler}
+                />
+                <Button
+                    className="btn btn-default btn-sm glyphicon glyphicon-trash"
+                    onClick={props.onDeleteHandler}
+                />
+            </div>
         </div>
     </div>
 );
--- src/components/title.js
+++ src/components/title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = () => (
     <div className="row">
-        <div className="col-sm-12 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
+        <div className="col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
             <div className="d-flex justify-content-center">
                 <h1>Priority tasks</h1>
             </div>
