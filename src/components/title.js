5dc49191aec4d446cab3f77389fed2b2a4d0902b
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-05 13:39:42 +0200

    #24 Create a component which just contains title "Priority tasks"

---
+++ src/components/title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => (
+    <div className="row">
+        <div className="col-sm-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+            <div className="nav nav-tabs nav-justified">
+                <h1>Priority tasks</h1>
+            </div>
+        </div>
+    </div>
+);
+
+export default Title;
