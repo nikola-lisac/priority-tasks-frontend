47793566fe30a871fb3ff4e8d60ae106f30dd4f4
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-09 23:51:19 +0200

    #27 Center footerPanel.js

--- src/containers/footerPanel.js
+++ src/containers/footerPanel.js
@@ -30,12 +30,12 @@ class Footer extends Component {
         let numberOfTasks = visibleUncompletedTasks.length;
         let currentDateTomorrow = this.getCurrentDate(this.props.filter);
         return (
-            <div className="row">
+            <div className="row align-items-center">
                 <div className="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 d-inline-flex">
-                    <div className="col-xs-6 d-flex align-item-centers panel footer-info">
+                    <div className="col-xs-6 d-flex align-items-center align-self-justify panel footer-info">
                         <p>{this.props.filter}: {currentDateTomorrow}</p>
                     </div>
-                    <div className="col-xs-6 d-flex align-item-start panel footer-tasks">
+                    <div className="col-xs-6 d-flex align-items-center align-self-justify panel footer-tasks">
                         <p>
                             {visibleUncompletedTasks.length}
                             {numberOfTasks > 1 ?
