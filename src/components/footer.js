3ee6831f4f0e3c51b35bdc939c10782eb8d23c47
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-09 03:18:01 +0200

    #27 Show day in Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+import moment from 'moment';
 
-const Footer = () => (
-    <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
-            <div className="d-flex justify-content-left panel panel-default">
-                tasks left
+class Footer extends Component {
+
+    render = () => {
+        return (
+            <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+                    <div className="d-flex justify-content-left panel">
+                        <h4>{moment(new Date()).format('dddd')}... 16 tasks left!</h4>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        )
+    }
+};
 
 export default Footer;
--- src/components/title.js
+++ src/components/title.js
@@ -4,7 +4,7 @@ const Title = () => (
     <div className="row">
         <div className="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
             <div className="d-flex justify-content-center app-title">
-                <h1>Priority tasks</h1>
+                <h3>Priority tasks</h3>
             </div>
         </div>
     </div>
