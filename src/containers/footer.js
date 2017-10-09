028d993366a0acc629b54cc1c8d86c2ff7f07b80
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-10 01:08:53 +0200

    #27 Reformat code

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -28,12 +28,12 @@ class Footer extends Component {
     render = () => {
         let visibleUncompletedTasks = this.getVisibleUncompletedTasks(this.props.tasks, this.props.filter);
         let numberOfTasks = visibleUncompletedTasks.length;
-        let currentDateTomorrow = this.getCurrentDate(this.props.filter);
+        let currentDate = this.getCurrentDate(this.props.filter);
         return (
             <div className="row align-items-center">
                 <div className="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 d-inline-flex">
                     <div className="col-xs-6 d-flex align-items-center align-self-justify panel footer-info">
-                        <p>{this.props.filter}: {currentDateTomorrow}</p>
+                        <p>{this.props.filter}: {currentDate}</p>
                     </div>
                     <div className="col-xs-6 d-flex align-items-center align-self-justify panel footer-tasks">
                         <p>
@@ -41,7 +41,7 @@ class Footer extends Component {
                             {numberOfTasks > 1 ?
                                 " tasks left" :
                                 numberOfTasks === 0 ?
-                                    " No more tasks...Congratulations!" :
+                                    " no more tasks...Congratulations!" :
                                     " task left"
                             }
                         </p>
