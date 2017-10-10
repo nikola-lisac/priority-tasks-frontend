0ecf5ecd84929901a6b831ad241c75f856009ba4
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-10 22:30:01 +0200

    #27 Show the number of completed tasks

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -15,6 +15,17 @@ class Footer extends Component {
                 throw new Error("Unknown filter: " + filter)
         }
     };
+
+    getVisibleCompletedTasks = (tasks, filter) => {
+        switch (filter) {
+            case "TODAY":
+                return tasks.filter(task => !task.postpone && task.completed);
+            case "TOMORROW":
+                return tasks.filter(task => task.postpone && task.completed);
+            default:
+                throw new Error("Unknown filter: " + filter)
+        }
+    };
     getCurrentDate = filter => {
         switch (filter) {
             case "TODAY":
@@ -27,19 +38,33 @@ class Footer extends Component {
     };
     render = () => {
         let visibleUncompletedTasks = this.getVisibleUncompletedTasks(this.props.tasks, this.props.filter);
+        let visibleCompletedTasks = this.getVisibleCompletedTasks(this.props.tasks, this.props.filter);
         let numberOfUncompletedTasks = visibleUncompletedTasks.length;
+        let numberOfCompletedTasks = visibleCompletedTasks.length;
         let currentDate = this.getCurrentDate(this.props.filter);
         return (
             <div className="row align-items-center">
                 <div className="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 d-inline-flex">
                     <div className="col-xs-6 d-flex justify-content-center panel footer-info">
-                        <p className="align-self-center">{this.props.filter}: {currentDate}</p>
+                        <p className="align-self-center">
+                            {this.props.filter}: {currentDate}
+                        </p>
                     </div>
-                    <div className="col-xs-6 d-flex justify-content-center panel footer-tasks">
+                    <div className="col-xs-3 d-flex justify-content-center panel footer-tasks-completed">
                         <p className="align-self-center">
                             {numberOfUncompletedTasks !== 0 ? visibleUncompletedTasks.length : ""}
-                            {numberOfUncompletedTasks === 0 ? "No more tasks...Congratulations!" :
+                            {numberOfUncompletedTasks === 0 && numberOfCompletedTasks === 0 ? "" :
+                                numberOfUncompletedTasks === 0 ? "No more... Congratulations !" :
                                 numberOfUncompletedTasks === 1 ? " task left" : " tasks left"
+                            }
+                        </p>
+                    </div>
+                    <div className="col-xs-3 d-flex justify-content-center panel footer-tasks-uncompleted">
+                        <p className="align-self-center">
+                            {numberOfCompletedTasks !== 0 ? visibleCompletedTasks.length : ""}
+                            {numberOfCompletedTasks === 0 && numberOfUncompletedTasks === 0 ? "" :
+                                numberOfCompletedTasks === 0 ? "Sorry, let's done at least one !" :
+                                    numberOfCompletedTasks === 1 ? " task done" : " tasks done"
                             }
                         </p>
                     </div>
