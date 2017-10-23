c3bd944df157508f512070bb53bb29eb4e6c06a1
Goran Gojkovic <gojkovic.goran73@gmail.com>
2017-10-23 09:21:27 +0200

    #master Delete border

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -4,7 +4,6 @@ const filterReducer = (state = "TODAY", action) => {
     switch (action.type) {
         case SET_VISIBILITY_FILTER :
             return action.filter;
-
         default:
             return state
     }
